e6bb76d3c718be1ae6c558a94bf6ea0994397fc5
EzequielMejiasMelo <ezequiel.mejiasmelo@gmail.com>
2022-03-04 22:47:01 -0500

    Contact Form

--- src/components/Cards/Card.js
+++ src/components/Cards/Card.js
@@ -1,9 +1,15 @@
 import React from "react";
 
-const Card = (card) => {
+const Card = ({card}) => {
     return (
         <div className="card w-100 h-100">
-            <img className="card-img-top image-card" alt="Screenshot of the application"></img>
+            <img className="card-img-top image-card" alt="Screenshot of the application" src={card.img_path} ></img>
+            <div className="card-body">
+                <h3 className="card-title text-white">{card.title}</h3>
+                <p className="card-text text-white">{card.description}</p>
+                <a href={card.github} target="_blank" className="btn btn-primary" rel="noreferrer">Github</a>
+                <a href={card.site} target="_blank" className="btn btn-success" rel="noreferrer">App</a>
+            </div>
         </div>
     )
 };
--- src/components/Cards/CardList.js
+++ src/components/Cards/CardList.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { cards } from "./cardData";
+import Card from "./Card";
 
 const CardList = () => {
-
-
- 
-
+    return (
+        <div className="works-cards content work mb-4">
+            {cards.map((card) => (
+                <Card card={card} />
+            ))}
+        </div>
+    )
 };
 
 export default CardList;
